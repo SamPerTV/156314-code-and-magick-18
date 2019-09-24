db870c422700e86e6d5c889e11f7b54f77e4cf8d
Sergey <sergey.m.683@gmail.com>
2019-09-24 14:46:38 +0300

    Добавляет цвета в бары,устанавливает очки,выводит статистику

--- js/stat.js
+++ js/stat.js
@@ -3,56 +3,76 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var CLOUD_X = 150;
-var CLOUD_Y = 90;
-var GAP = 90;
-var FONT_GAP = 5;
-var TEXT_WIDTH = 50;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_GAP = 10;
 
+var BAR_X = 160;
+var GAP = 90;
+var FONT_GAP = 30;
+var GAP_SCORE = 5;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
-var NAME_X = 160;
+var NAME_X = 195;
 var NAME_Y = 270;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
+
   var maxElement = arr[0];
-  
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
+
       maxElement = arr[i];
     }
   }
-  
   return maxElement;
 };
 
-/* var renderBackgroundColor = function() {
-    var fixColorPlayers = 
- if(playres == ['Вы']) {
-     
- }
-}
-*/
-window.renderStatistics = function(ctx, players, times) {
+var renderText = function (ctx) {
 
-  renderCloud(ctx, 110,20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
   ctx.fillStyle = '#000';
-  
-    var players = ['Вы', 'Иван', 'Юлия', 'Кекс'];
-    var maxTime = getMaxElement(times);
-    
-    for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], NAME_X + GAP * i - FONT_GAP, NAME_Y ,);
-    ctx.fillRect(CLOUD_X + GAP * i, CLOUD_Y, BAR_WIDTH, BAR_HEIGHT);
-    
-  }
+  ctx.font = 'PT Mono 16px';
+  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP);
+  ctx.fillText('Cписок результатов', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP * 2);
+
+};
+var getRandomNumber = function (max) {
+  return Math.floor(Math.random() * max + 1);
 };
 
-//(BAR_HEIGHT * times[i]) / maxTime)
+var getBarColor = function (playerName) {
+  var bgColor = 'hsl(235, ' + getRandomNumber(100) + '%, 50%)';
+  if (playerName === 'Вы') {
+    bgColor = 'rgba(255, 0, 0, 1) ';
+  }
+  return bgColor;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderText(ctx);
+
+  players = ['Вы', 'Иван', 'Юлия', 'Кекс'];
+  var maxTime = getMaxElement(times);
+  var scoreBar;
+  for (var i = 0; i < players.length; i++) {
+    var height = BAR_HEIGHT * times[i] / maxTime;
+    var bgColor = getBarColor(players[i]);
+    var BAR_Y = CLOUD_Y + GAP + (BAR_HEIGHT - height);
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], NAME_X + GAP * i - FONT_GAP, NAME_Y);
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(BAR_X + GAP * i, BAR_Y, BAR_WIDTH, height);
+    ctx.fillStyle = '#000';
+    scoreBar = Math.floor(height);
+    ctx.fillText(scoreBar, BAR_X + GAP_SCORE + GAP * i, BAR_Y - GAP_SCORE);
+  }
+};
